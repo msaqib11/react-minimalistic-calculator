f0435fb615572c10d915ce697b6f4fe1298f4d23
msaqib11 <saqib.maverick@gmail.com>
2024-08-29 12:23:11 -0700

    added footer notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Calculator from './components/calculator'
+import Footer from './components/footer/Index'
 
 function App() {
   return <>
     <Calculator/>
+    <Footer/>
   </>
 }
 
---
+++ src/components/footer/Index.jsx
@@ -0,0 +1,7 @@
+function Footer(){
+return(<>
+<h1 className="text-left ml-4 text-slate-700 text-lg">Powered by <a href="https://www.linkedin.com/in/msaqib11/" target="_blank">Muhammad saqib</a></h1>
+</>)
+}
+
+export default Footer
